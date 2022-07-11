8ca24ec32f9561b8dd6a60fa7300fa3a68412065
Tiago <tiago.salles@houseasy.net>
2022-07-11 18:58:16 -0300

    Feat: Added new book and the reading is working

--- app/main.py
+++ app/main.py
@@ -1,4 +1,11 @@
 import pyttsx3
+import PyPDF2
+
+book = open("Advanced C.pdf", "rb")
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
 speaker = pyttsx3.init()
-speaker.say("Hello World")
+page = pdfReader.getPage(181)
+pageContent = page.extractText()
+speaker.say(pageContent)
 speaker.runAndWait()
