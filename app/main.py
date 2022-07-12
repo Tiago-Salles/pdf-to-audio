4c537657e5ff77a1dcd2568dc577081e201ce532
Tiago <tiago.salles@houseasy.net>
2022-07-12 08:21:56 -0300

    Feat: Adjusted voice rate and language to English

--- app/main.py
+++ app/main.py
@@ -5,6 +5,11 @@ book = open("Advanced C.pdf", "rb")
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 speaker = pyttsx3.init()
+
+for voice in speaker.getProperty("voices"):
+    if(voice.id == "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"):
+        speaker.setProperty("voice", voice.id)
+speaker.setProperty("rate", 125)
 page = pdfReader.getPage(181)
 pageContent = page.extractText()
 speaker.say(pageContent)
