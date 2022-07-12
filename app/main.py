0e63b45af38d7ccc11b0ba71faf191b486beabe1
Tiago <tiago.salles@houseasy.net>
2022-07-12 09:09:20 -0300

    Feat: Implemented reading the full file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import pyttsx3
 import PyPDF2
 
-book = open("Advanced C.pdf", "rb")
+book = open("Einstein_Relativity.pdf", "rb")
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 speaker = pyttsx3.init()
@@ -9,8 +9,11 @@ speaker = pyttsx3.init()
 for voice in speaker.getProperty("voices"):
     if(voice.id == "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"):
         speaker.setProperty("voice", voice.id)
-speaker.setProperty("rate", 125)
-page = pdfReader.getPage(181)
-pageContent = page.extractText()
-speaker.say(pageContent)
+speaker.setProperty("rate", 150)
+
+for page in range(6, pages):
+    _page = pdfReader.getPage(page)
+    pageContent = _page.extractText()
+    speaker.say(pageContent)
+
 speaker.runAndWait()
